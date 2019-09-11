bac3ec7eb0f77c600050d5d94a7f95b77af99b07
Teo <teo.budeanu@gmail.com>
2019-09-11 15:25:10 +0300

    Register Account Model in admin site

--- pizza/accounts/admin.py
+++ pizza/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Account
 
 # Register your models here.
+@admin.register(Account)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'phone', 'user_id')
+    list_editable = ('phone', 'user_id')
