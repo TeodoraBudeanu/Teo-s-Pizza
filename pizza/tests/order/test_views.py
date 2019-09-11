33e5b3eaa08cae54128d535ba1f0728187098827
Teo <teo.budeanu@gmail.com>
2019-09-11 11:37:32 +0300

    Delete unused imports, cleanup order/test_views.py

--- pizza/tests/order/test_views.py
+++ pizza/tests/order/test_views.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from unittest import skip
 from django.utils import timezone
 from order.models import Order, Pizza
 from django.contrib.auth.models import User
@@ -10,27 +9,27 @@ class OrderViewsAnonymousUserTest(TestCase):
     # python3 manage.py test tests.order.test_views.OrderViewsAnonymousUserTest
     def test_user_is_redirected_to_login_when_tries_to_place_order(self):
         response = self.client.get('/order/place_order')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/place_order',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/place_order')
 
     def test_user_is_redirected_to_login_when_tries_to_save_order(self):
         response = self.client.get('/order/save_order')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/save_order',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/save_order')
 
     def test_user_is_redirected_to_login_when_tries_to_get_confirm_order(self):
         response = self.client.get('/order/confirm_order')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/confirm_order',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/confirm_order')
 
     def test_user_is_redirected_to_login_when_tries_to_confirm_order(self):
         response = self.client.post('/order/confirm_order')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/confirm_order',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/confirm_order')
 
 
 class OrderViewsLoggedInUserTest(TestCase):
@@ -53,46 +52,46 @@ class OrderViewsLoggedInUserTest(TestCase):
 
     def test_place_order(self):
         self.response = self.client.get('/order/place_order')
-        self.assertEquals(200, self.response.status_code)
+        self.assertEquals(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'form.html')
 
     def test_when_user_places_order_a_new_order_is_created(self):
         # if an Open/Confirmed order doesn't already exist
         Order.objects.all().delete()
         self.response = self.client.get('/order/place_order')
-        self.assertEquals(1, Order.objects.all().count())
+        self.assertEquals(Order.objects.all().count(), 1)
 
     def test_when_user_places_order_status_updates_to_open(self):
         # if an Open/Confirmed order exists
         self.order.status = 'C'
         self.order.save()
-        self.assertEquals('C', Order.objects.get().status)
+        self.assertEquals(Order.objects.get().status, 'C')
         self.response = self.client.get('/order/place_order')
-        self.assertEquals('O', Order.objects.get().status)
+        self.assertEquals(Order.objects.get().status, 'O')
 
     def test_save_order(self):
         self.response = self.client.get('/order/save_order',
                                         {'order_id': 1,
                                          'address': 'test_address'})
-        self.assertEquals(200, self.response.status_code)
+        self.assertEquals(self.response.status_code, 200)
 
     def test_when_user_saves_order_address_is_updated(self):
         self.response = self.client.get('/order/save_order',
                                         {'order_id': 1,
                                          'address': 'test_address'})
         self.order.refresh_from_db()
-        self.assertEquals('test_address', self.order.address)
+        self.assertEquals(self.order.address, 'test_address')
 
     def test_when_user_saves_order_comment_is_updated(self):
         self.response = self.client.get('/order/save_order',
                                         {'order_id': 1,
                                          'comment': 'test_comment'})
         self.order.refresh_from_db()
-        self.assertEquals('test_comment', self.order.comment)
+        self.assertEquals(self.order.comment, 'test_comment')
 
     def test_confirm_order_get(self):
         self.response = self.client.get('/order/confirm_order')
-        self.assertEquals(200, self.response.status_code)
+        self.assertEquals(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'confirmation.html')
 
     def test_confirm_order_post(self):
@@ -104,14 +103,14 @@ class OrderViewsLoggedInUserTest(TestCase):
     def test_when_user_confirms_order_status_updates_to_confirmed(self):
         self.response = self.client.get('/order/confirm_order')
         self.order.refresh_from_db()
-        self.assertEquals('C', self.order.status)
+        self.assertEquals(self.order.status, 'C')
 
     def test_when_user_pays_order_status_updates_to_paid(self):
         self.order.status = 'C'
         self.order.save()
         self.response = self.client.post('/order/confirm_order')
         self.order.refresh_from_db()
-        self.assertEquals('P', self.order.status)
+        self.assertEquals(self.order.status, 'P')
 
     def test_when_user_pays_order_date_updates_to_current(self):
         self.order.status = 'C'
@@ -130,20 +129,20 @@ class OrderViewsLoggedInUserTest(TestCase):
 
     def test_check_total_returns_order_amount(self):
         self.response = self.client.get('/order/check_total', {'order_id': 1})
-        self.assertEquals('50', self.response.content.decode("utf-8"))
+        self.assertEquals(self.response.content.decode("utf-8"), '50')
 
     def test_when_non_existent_order_id_is_provided_check_total_raises_404(self):
         self.response = self.client.get('/order/check_total', {'order_id': 2})
-        self.assertEquals(404, self.response.status_code)
+        self.assertEquals(self.response.status_code, 404)
 
     def test_order_details(self):
         self.response = self.client.get('/order/order_details/1/')
-        self.assertEquals(200, self.response.status_code)
+        self.assertEquals(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'odetails.html')
 
     def test_order_details_returns_order(self):
         self.response = self.client.get('/order/order_details/1/')
-        self.assertEquals(1, self.response.context['order'].id)
+        self.assertEquals(self.response.context['order'].id, 1)
 
     def test_order_details_raises_PermissionDenied_to_user(self):
         user = User(username="bobby", first_name="Bob", last_name="Dylan",
@@ -155,22 +154,22 @@ class OrderViewsLoggedInUserTest(TestCase):
         self.order.save()
         self.response = self.client.force_login(user)
         self.response = self.client.get('/order/order_details/1/')
-        self.assertEquals(403, self.response.status_code)
+        self.assertEquals(self.response.status_code, 403)
 
     def test_when_non_existent_order_id_is_provided_order_details_raises_404(self):
         self.response = self.client.get('/order/order_details/2/')
-        self.assertEquals(404, self.response.status_code)
+        self.assertEquals(self.response.status_code, 404)
 
     def test_order_list(self):
         self.response = self.client.get('/order/history')
-        self.assertEquals(200, self.response.status_code)
+        self.assertEquals(self.response.status_code, 200)
         self.assertTemplateUsed(self.response, 'history.html')
 
     def test_order_list_returns_list(self):
         self.order.status = "P"
         self.order.save()
         self.response = self.client.get('/order/history')
-        self.assertEquals(1, len(self.response.context['orders']))
+        self.assertEquals(len(self.response.context['orders']), 1)
 
 
 class OrderItemViewsAnonymousUserTest(TestCase):
@@ -178,21 +177,21 @@ class OrderItemViewsAnonymousUserTest(TestCase):
     # test tests.order.test_views.OrderItemViewsAnonymousUserTest
     def test_user_is_redirected_to_login_when_tries_to_save_item(self):
         response = self.client.get('/order/save_item')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/save_item',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/save_item')
 
     def test_user_is_redirected_to_login_when_tries_to_create_item(self):
         response = self.client.get('/order/save_item')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/save_item',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/save_item')
 
     def test_user_is_redirected_to_login_when_tries_to_delete_item(self):
         response = self.client.get('/order/save_item')
-        self.assertEquals(302, response.status_code)
-        self.assertEquals('/accounts/login/?next=/order/save_item',
-                          response.url)
+        self.assertEquals(response.status_code, 302)
+        self.assertEquals(response.url,
+                          '/accounts/login/?next=/order/save_item')
 
 
 class OrderItemViewsLoggedInUserTest(TestCase):
@@ -214,7 +213,7 @@ class OrderItemViewsLoggedInUserTest(TestCase):
                                         {'item_id': 1, 'pizza_id': 1,
                                          'quantity': 0})
         self.order_item.refresh_from_db()
-        self.assertEquals(1, self.order_item.pizza_type.id)
+        self.assertEquals(self.order_item.pizza_type.id, 1)
 
     def test_order_item_is_saved_when_new_quantity_is_provided(self):
         self.order_item.pizza_type = Pizza.objects.get()
@@ -223,33 +222,33 @@ class OrderItemViewsLoggedInUserTest(TestCase):
                                         {'item_id': 1, 'pizza_id': 1,
                                          'quantity': 3})
         self.order_item.refresh_from_db()
-        self.assertEquals(3, self.order_item.quantity)
+        self.assertEquals(self.order_item.quantity, 3)
 
     def test_when_non_existent_id_is_provided_save_item_raises_404(self):
         self.response = self.client.get('/order/save_item',
                                         {'item_id': 2, 'pizza_id': 1,
                                          'quantity': 3})
-        self.assertEquals(404, self.response.status_code)
+        self.assertEquals(self.response.status_code, 404)
 
     def test_when_non_existent_pizza_id_is_provided_save_item_raises_404(self):
         self.response = self.client.get('/order/save_item',
                                         {'item_id': 1, 'pizza_id': 2,
                                          'quantity': 3})
-        self.assertEquals(404, self.response.status_code)
+        self.assertEquals(self.response.status_code, 404)
 
     def test_when_provided_quantity_is_higher_than_stock_raises_400(self):
         self.response = self.client.get('/order/save_item',
                                         {'item_id': 1, 'pizza_id': 1,
                                          'quantity': 20})
-        self.assertEquals(400, self.response.status_code)
+        self.assertEquals(self.response.status_code, 400)
 
     def test_create_item(self):
         self.assertEquals(1, self.order.order_items.all().count())
         self.response = self.client.get('/order/create_item',
                                         {'old_item_id': 1})
-        self.assertEquals(2, self.order.order_items.all().count())
+        self.assertEquals(self.order.order_items.all().count(), 2)
 
     def test_delete_item(self):
-        self.assertEquals(1, self.order.order_items.all().count())
+        self.assertEquals(self.order.order_items.all().count(), 1)
         self.response = self.client.get('/order/delete_item', {'item_id': 1})
-        self.assertEquals(0, self.order.order_items.all().count())
+        self.assertEquals(self.order.order_items.all().count(), 0)
